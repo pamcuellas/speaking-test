f638f75e963e2e892e99dfa39ad9d6888f6607a1
pamcuellas <pamcuellas@gmail.com>
2019-09-04 19:23:33 -0400

    Timer for preparation time added.

--- assets/js/customizing.js
+++ assets/js/customizing.js
@@ -8,15 +8,15 @@ $(document).ready( function() { 	// Wait until DOM be ready to start.
         qPreparationTime: [0],
         qRecordingTime:30000,
         qBarrMSGDuration:90,
-        qBarrPrepDuration:3,
+        qBarrPrepDuration:30,
         qImage:["Q1/Q01-1.png","Q1/Q01-2.png","Q1/Q01-3.png"],
         qDescription:"Give me an advice."},
     {qNumber: 2, 
         qName:"Talking about a personal experience",
         qPreparationTime: [120000],
-        qRecordingTime:150000, /* 90000*/ 
+        qRecordingTime:150000,  
         qBarrMSGDuration:90,
-        qBarrPrepDuration:3,
+        qBarrPrepDuration:30,
         qImage:["Q2/Q02-1.png","Q2/Q02-2.png","Q2/Q02-3.png"],
         qDescription:"Speak about the best day of work you have ever had."},
     {qNumber: 3, 
@@ -24,7 +24,7 @@ $(document).ready( function() { 	// Wait until DOM be ready to start.
         qPreparationTime: [240000],
         qRecordingTime:270000,
         qBarrMSGDuration:60,
-        qBarrPrepDuration:3,
+        qBarrPrepDuration:30,
         qImage:["Q3/Q03-1.png","Q3/Q03-2.png","Q3/Q03-3.png"],
         qDescription:"Speak about the best day of work you have ever had."},
     {qNumber: 4, 
@@ -32,7 +32,7 @@ $(document).ready( function() { 	// Wait until DOM be ready to start.
         qPreparationTime: [330000],
         qRecordingTime:360000,
         qBarrMSGDuration:60,
-        qBarrPrepDuration:3,
+        qBarrPrepDuration:30,
         qImage:["Q4/Q04-1.png","Q4/Q04-2.png","Q4/Q04-3.png"],
         qDescription:"Give me the next lottery numbers."},
     {qNumber: 5, 
@@ -40,7 +40,7 @@ $(document).ready( function() { 	// Wait until DOM be ready to start.
         qPreparationTime: [420000, 480000],
         qRecordingTime:540000,
         qBarrMSGDuration:60,
-        qBarrPrepDuration:6,
+        qBarrPrepDuration:60,
         qImage:["Q5/Q05-1-A.png","Q5/Q05-2-A.png","Q5/Q05-3-A.png"],
         qDescription:"Try to convince me."},
     {qNumber: 6, 
@@ -48,7 +48,7 @@ $(document).ready( function() { 	// Wait until DOM be ready to start.
         qPreparationTime: [600000],
         qRecordingTime:660000,
         qBarrMSGDuration:60,
-        qBarrPrepDuration:6,
+        qBarrPrepDuration:60,
         qImage:["Q6/Q06-1.png","Q6/Q06-2.png","Q6/Q06-3.png"],
         qDescription:"Try to convince me."},
     {qNumber: 7, 
@@ -56,7 +56,7 @@ $(document).ready( function() { 	// Wait until DOM be ready to start.
         qPreparationTime: [720000],
         qRecordingTime:750000,
         qBarrMSGDuration:90,
-        qBarrPrepDuration:3,
+        qBarrPrepDuration:30,
         qImage:["Q7/Q07-1.png","Q7/Q07-2.png","Q7/Q07-3.png"],
         qDescription:"What to you thing about Donald Trump."},
     {qNumber: 8, 
@@ -64,24 +64,36 @@ $(document).ready( function() { 	// Wait until DOM be ready to start.
         qPreparationTime: [840000],
         qRecordingTime:870000,
         qBarrMSGDuration:60,
-        qBarrPrepDuration:3,
+        qBarrPrepDuration:30,
         qImage:["Q8/Q08-1.png","Q8/Q08-2.png","Q8/Q08-3.png"],
         qDescription:"Tell me about your mother-in-law."}
     ];
 
     var rootDirImage = "./assets/images/";
     let pos = 0;
-    let posImageQ3 = 0;   
-    $( ".progress-bar" ).css({ animationName:"none", width:"0%"});
+    let posImageQ3 = 0;
 
-    sctructure.forEach( ( value, index ) =>  {
+    function timer( time ) {
+        var x = setInterval( () => {
+            time -= 1;  
+            $("#timer").text("Preparation time: " + time + "s ");
+            if (time <= 0) {
+                $("#timer").text("");
+                clearInterval(x);
+            }
+        }, 1000);
+    }
+
+    sctructure.forEach( ( value ) =>  {
+        
         /* Set Preparation Time */ 
         setTimeout( () => {
+            timer( value.qBarrPrepDuration );
             pos = Math.floor(Math.random() * value.qImage.length);
             $(".msg").text("");
             $(".title").text("Question number " + value.qNumber);
 
-            $(".progress-bar").css({ animationName:"none", width:"0%"});
+            $("#prgss-01 .progress-bar").css({ animationName:"none", width:"0%"});
 
             if (value.qNumber != 4) {
                 $("#image").prop("src", rootDirImage + value.qImage[pos]);
@@ -89,10 +101,8 @@ $(document).ready( function() { 	// Wait until DOM be ready to start.
                 /* Question 4 - Must corresponding to image question 3 */
                 $("#image").prop("src", rootDirImage + value.qImage[posImageQ3]);
             }
-
             /* Load image 3 position in array to be used on image 4 */ 
             posImageQ3 =  pos;
-
         }, value.qPreparationTime[0]);
 
         /* Question 5 - customized */
@@ -102,17 +112,11 @@ $(document).ready( function() { 	// Wait until DOM be ready to start.
                 $("#image").prop("src", rootDirImage + image);
             }, value.qPreparationTime[1]);
         }
-
-        /* Set Recording Time */
-        // setTimeout( () => {  
-        //     var a = 1;
-        //     $(".msg").text("Start speaking now !!!"); 
-        // }, value.qRecordingTime-10 ) ;
         
+        /* Speaking part */ 
         setTimeout( () => {
             $(".msg").text("Start speaking now !!!"); 
-            // $( ".progress-bar" ).css({ animationName:"none"});
-            $( ".progress-bar" ).css({animationName:"barr", width:"100%", animationDuration: value.qBarrMSGDuration + "s"});
+            $( "#prgss-01 .progress-bar" ).css({animationName:"barr", width:"100%", animationDuration: value.qBarrMSGDuration + "s"});
         }, value.qRecordingTime);
     });
 
