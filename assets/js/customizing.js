3cfed2196d26eb0eb27caed044bdfb3e24ed6836
pamcuellas <pamcuellas@gmail.com>
2019-09-05 10:12:54 -0400

    Images size adjusted and speaking-test.html page added.

--- assets/js/customizing.js
+++ assets/js/customizing.js
@@ -4,67 +4,67 @@ $(document).ready( function() { 	// Wait until DOM be ready to start.
 
     var sctructure = [
     {qNumber:1, 
-        qName:"Giving Advice",
+        qName:"Give an Advice",
         qPreparationTime: [0],
-        qRecordingTime:30000,
-        qBarrMSGDuration:90,
-        qBarrPrepDuration:30,
+        qRecordingTime:3000,
+        qBarrMSGDuration:9,
+        qBarrPrepDuration:3,
         qImage:["Q1/Q01-1.png","Q1/Q01-2.png","Q1/Q01-3.png"],
         qDescription:"Give me an advice."},
     {qNumber: 2, 
-        qName:"Talking about a personal experience",
-        qPreparationTime: [120000],
-        qRecordingTime:150000,  
-        qBarrMSGDuration:90,
-        qBarrPrepDuration:30,
+        qName:"Talk about a personal experience",
+        qPreparationTime: [12000],
+        qRecordingTime:15000,  
+        qBarrMSGDuration:9,
+        qBarrPrepDuration:3,
         qImage:["Q2/Q02-1.png","Q2/Q02-2.png","Q2/Q02-3.png"],
         qDescription:"Speak about the best day of work you have ever had."},
     {qNumber: 3, 
-        qName:"Describing a scene",
-        qPreparationTime: [240000],
-        qRecordingTime:270000,
-        qBarrMSGDuration:60,
-        qBarrPrepDuration:30,
+        qName:"Describe this scene",
+        qPreparationTime: [24000],
+        qRecordingTime:27000,
+        qBarrMSGDuration:6,
+        qBarrPrepDuration:3,
         qImage:["Q3/Q03-1.png","Q3/Q03-2.png","Q3/Q03-3.png"],
         qDescription:"Speak about the best day of work you have ever had."},
     {qNumber: 4, 
-        qName:"Making Predictions",
-        qPreparationTime: [330000],
-        qRecordingTime:360000,
-        qBarrMSGDuration:60,
-        qBarrPrepDuration:30,
+        qName:"Make Predictions",
+        qPreparationTime: [33000],
+        qRecordingTime:36000,
+        qBarrMSGDuration:6,
+        qBarrPrepDuration:3,
         qImage:["Q4/Q04-1.png","Q4/Q04-2.png","Q4/Q04-3.png"],
         qDescription:"Give me the next lottery numbers."},
     {qNumber: 5, 
-        qName:"Comparing and persuading",
-        qPreparationTime: [420000, 480000],
-        qRecordingTime:540000,
-        qBarrMSGDuration:60,
-        qBarrPrepDuration:60,
+        qName:"Compare and persuade",
+        qPreparationTime: [42000, 48000],
+        qRecordingTime:54000,
+        qBarrMSGDuration:6,
+        qBarrPrepDuration:6,
         qImage:["Q5/Q05-1-A.png","Q5/Q05-2-A.png","Q5/Q05-3-A.png"],
         qDescription:"Try to convince me."},
     {qNumber: 6, 
-        qName:"Dealing with a difficult situation",
-        qPreparationTime: [600000],
-        qRecordingTime:660000,
-        qBarrMSGDuration:60,
-        qBarrPrepDuration:60,
+        qName:"Deal with a difficult situation",
+        qPreparationTime: [60000],
+        qRecordingTime:66000,
+        qBarrMSGDuration:6,
+        qBarrPrepDuration:6,
         qImage:["Q6/Q06-1.png","Q6/Q06-2.png","Q6/Q06-3.png"],
         qDescription:"Try to convince me."},
     {qNumber: 7, 
-        qName:"Expressing opinions",
-        qPreparationTime: [720000],
-        qRecordingTime:750000,
-        qBarrMSGDuration:90,
-        qBarrPrepDuration:30,
+        qName:"Opinion",
+        qPreparationTime: [72000],
+        qRecordingTime:75000,
+        qBarrMSGDuration:9,
+        qBarrPrepDuration:3,
         qImage:["Q7/Q07-1.png","Q7/Q07-2.png","Q7/Q07-3.png"],
         qDescription:"What to you thing about Donald Trump."},
     {qNumber: 8, 
         qName:"Describing an unusual situation",
-        qPreparationTime: [840000],
-        qRecordingTime:870000,
-        qBarrMSGDuration:60,
-        qBarrPrepDuration:30,
+        qPreparationTime: [84000],
+        qRecordingTime:87000,
+        qBarrMSGDuration:6,
+        qBarrPrepDuration:3,
         qImage:["Q8/Q08-1.png","Q8/Q08-2.png","Q8/Q08-3.png"],
         qDescription:"Tell me about your mother-in-law."}
     ];
@@ -73,10 +73,10 @@ $(document).ready( function() { 	// Wait until DOM be ready to start.
     let pos = 0;
     let posImageQ3 = 0;
 
-    function timer( time ) {
+    function timer( time, currText ) {
         var x = setInterval( () => {
             time -= 1;  
-            $("#timer").text("Preparation time: " + time + "s ");
+            $("#timer").text(currText + time + "s ");
             if (time <= 0) {
                 $("#timer").text("");
                 clearInterval(x);
@@ -85,15 +85,16 @@ $(document).ready( function() { 	// Wait until DOM be ready to start.
     }
 
     sctructure.forEach( ( value ) =>  {
-        
+
         /* Set Preparation Time */ 
         setTimeout( () => {
-            timer( value.qBarrPrepDuration );
+            var currText = (value.qNumber != 5) ? "Preparation time: " : "Selection time: ";
+            timer( value.qBarrPrepDuration, currText );
             pos = Math.floor(Math.random() * value.qImage.length);
             $(".msg").text("");
             $(".title").text("Question number " + value.qNumber);
-
-            $("#prgss-01 .progress-bar").css({ animationName:"none", width:"0%"});
+            $(".subject").text(value.qName);
+            $(".progress-bar").css({animationName:"none", width:"0%"});
 
             if (value.qNumber != 4) {
                 $("#image").prop("src", rootDirImage + value.qImage[pos]);
@@ -108,6 +109,7 @@ $(document).ready( function() { 	// Wait until DOM be ready to start.
         /* Question 5 - customized */
         if (value.qNumber === 5) {
             setTimeout( () => {
+                timer( value.qBarrPrepDuration, "Preparation time: " );
                 let image = value.qImage[pos].replace("A","B");
                 $("#image").prop("src", rootDirImage + image);
             }, value.qPreparationTime[1]);
@@ -115,8 +117,8 @@ $(document).ready( function() { 	// Wait until DOM be ready to start.
         
         /* Speaking part */ 
         setTimeout( () => {
-            $(".msg").text("Start speaking now !!!"); 
-            $( "#prgss-01 .progress-bar" ).css({animationName:"barr", width:"100%", animationDuration: value.qBarrMSGDuration + "s"});
+            $(".msg").text("Start speaking now !"); 
+            $( ".progress-bar" ).css({animationName:"barr", width:"100%", animationDuration: value.qBarrMSGDuration + "s"});
         }, value.qRecordingTime);
     });
 
